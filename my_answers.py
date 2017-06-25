2893a4a4f16e61d95d7f7722c9e27f94c9aecb67
Shuping <shuping.ruan@gmail.com>
2017-06-24 23:43:26 -0400

    update my_answer.py

--- my_answers.py
+++ my_answers.py
@@ -24,24 +24,54 @@ def window_transform_series(series,window_size):
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
-def build_part1_RNN(step_size, window_size):
-    pass
+def build_part1_RNN(window_size):
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+    # build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
 
+    # compile the model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+    return model
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
     # find all unique characters in the text
-
-
-    # remove as many non-english characters and character sequences as you can 
+    # remove as many non-english characters and character sequences as you can
+    text = text.replace('\n', ' ')  # replacing '\n' with '' simply removes the sequence
+    text = text.replace('\r', ' ')
+    text = text.replace('\ufeff', ' ')
+    re.sub(r'\s', ' ', text)  # replace all white space[\t\n\r\f]
+    text = text.replace('à', 'a')
+    text = text.replace('â', 'a')
+    text = text.replace('è', 'e')
+    text = text.replace('é', 'e')
+    text = text.replace('@', ' ')
+    text = text.replace('*', ' ')
+    text = text.replace('&', ' ')
+    text = text.replace('#', ' ')
+    text = text.replace('$', ' ')
+    text = text.replace('/', ' ')
+    return text
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text,window_size,step_size):
     # containers for input/output pairs
-    inputs = []
-    outputs = []
-    
-
-    
+    length = len(text) # obtain the length of the series
+    inputs = [ text[i: i+window_size] for i in range(length-window_size) ]
+    outputs =[ text[i+window_size] for i in range(length-window_size) ]
     return inputs,outputs
+
+# TODO build the required RNN model: a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
+def build_part2_RNN(window_size):
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size, len(chars))))
+    model.add(Dense(len(chars), activation='softmax'))
+    # initialize optimizer
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+    # compile model --> make sure initialized optimizer and callbacks - as defined above - are used
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+    return model
